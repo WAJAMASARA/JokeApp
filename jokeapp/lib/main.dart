e57546f65b39d83cba9b2eb236ad524f46ee94ea
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-12-01 02:28:43 +0530

    added the button and make the UI for the joke app Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- jokeapp/lib/main.dart
+++ jokeapp/lib/main.dart
@@ -1,125 +1,148 @@
 import 'package:flutter/material.dart';
+import 'joke_service.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Fit 21'),
+      title: 'Joke App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const JokeListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class JokeListPage extends StatefulWidget {
+  const JokeListPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _JokeListPageState createState() => _JokeListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _JokeListPageState extends State<JokeListPage> {
+  final JokeService _jokeService = JokeService();
+  List<Map<String, dynamic>> _jokesRaw = [];
+  bool _isLoading = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> _fetchJokes() async {
+    setState(() => _isLoading = true);
+    try {
+      _jokesRaw = await _jokeService.fetchJokesRaw();
+      setState(() => _isLoading = false);
+    } catch (e) {
+      throw Exception(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text(
+          'Joke App',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+          ),
+        ),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.deepPurple.shade100, Colors.white],
+            )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Welcome to the Joke App!',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepPurple,
+                    shadows: [Shadow(color: Colors.white, blurRadius: 2)]
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Click the button to fetch random jokes!',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.deepPurple,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: () {_fetchJokes();},
+                  style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll<Color>(Colors.deepPurple),
+                      minimumSize: WidgetStatePropertyAll<Size>(Size(0, 50)),
+                      shape: WidgetStatePropertyAll(LinearBorder.none),
+                      enableFeedback: true
+                  ),
+                  child: const Text(
+                    'Fetch Jokes',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )),
+              const SizedBox(height: 24),
+              Expanded(
+                child: _isLoading ?
+                const Center(child: CircularProgressIndicator())
+                    : _buildJokeList(),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+
+  Widget _buildJokeList() {
+    if (_jokesRaw.isEmpty) {
+      return const Center(
+        child: Text(
+          'No jokes fetched yet.',
+          style: TextStyle(fontSize: 18, color: Colors.deepPurple),
+        ),
+      );
+    }
+    return ListView.builder(
+      itemCount: _jokesRaw.length,
+      itemBuilder: (context, index) {
+        final jokeJson = _jokesRaw[index];
+        return Card(
+          margin: const EdgeInsets.only(bottom: 16),
+          elevation: 4,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                jsonEncode(jokeJson),
+                style: const TextStyle(fontSize: 14),
+              )
+          ),
+        );
+      },
     );
   }
 }
